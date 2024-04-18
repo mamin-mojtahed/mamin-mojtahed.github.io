991c54377a399e0d2dc8c44265c3acf387675bf5
Amin Mojtahed <amin.mojtahed@uwaterloo.ca>
2024-04-17 23:43:03 -0400

    added transitions

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,13 @@
 export function hoverHandler() {
   const emotElem = document.getElementById("emoticon");
   if (!emotElem) throw new Error("Element not found");
-  emotElem.hidden = false;
+  emotElem.classList.add("revealed");
 }
 export function leaveHandler() {
   // event.stopImmediatePropagation();
   const emotElem = document.getElementById("emoticon");
   if (!emotElem) throw new Error("Element not found");
-  emotElem.hidden = true;
+  emotElem.classList.remove("revealed");
 }
 document.querySelectorAll("header *").forEach((elem) => {
   elem.addEventListener("mouseenter", hoverHandler);
